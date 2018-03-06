8d6867c87fd4de25fc5a2c8952933d4130d0e3c1
Daniel Vella <vella-d@ulster.ac.uk>
2018-03-06 19:53:01 +0100

    Folder structure, app architecture and landing page

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-print("test");
+var express       = require("express"),
+    app           = express();
+
+// ROUTES
+var indexRoutes   = require("./routes/index");
+
+app.set("view engine", "ejs");
+
+// ROUTES
+app.use("/", indexRoutes);
+
+app.listen(3000, function(){
+    console.log("App is running");
+})
